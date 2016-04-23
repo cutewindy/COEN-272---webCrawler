3cc080e1d0b8ddfabe3c632fdaabf7cef908e314
cutewindy <wendi@WindydeMacBook-Air.local>
2016-04-23 01:37:37 -0700

    before Apr 23

--- src/webCrawler/Crawler.java
+++ src/webCrawler/Crawler.java
@@ -20,7 +20,7 @@ import java.util.concurrent.TimeUnit;
 public class Crawler {
 	
 	
-	private static final int MAX_PAGE_TO_SEARCH = 1;
+	private static final int MAX_PAGE_TO_SEARCH = 100;
 		
 	private Set<String> pagesVisited = new HashSet<String>();
 	private List<String> pagesToVisisted = new LinkedList<String>();
@@ -83,3 +83,91 @@ public class Crawler {
 	}
 
 }
+
+
+
+//package webCrawler;
+//
+//import java.io.File;
+//import java.util.List;
+//import java.util.HashSet;
+//import java.util.LinkedList;
+//import java.util.Set;
+//import java.util.concurrent.TimeUnit;
+//
+///**
+// * 1 Retrieve a web page from a website
+// * 2 Collect all the links on that page
+// * 3 Collect all the words on that page(not done)
+// * 4 Visit the next link
+// * 5 Keep track of pages that already visited
+// * 6 Put a limit on the number of pages to search so this doesn't run for eternity
+// * @author Wendi
+// * @return
+// */
+//public class Crawler {
+//	
+//	
+//	private static final int MAX_PAGE_TO_SEARCH = 10;
+//		
+//	private Set<String> pagesVisited = new HashSet<String>();
+//	private List<String> pagesToVisisted = new LinkedList<String>();
+//	
+//	
+//	/**
+//	 * Create CrawlerLeg to crawl the web page and collect all the links on that page
+//	 * @param url
+//	 */
+//	public void run(String url) {
+//		while (this.pagesVisited.size() < MAX_PAGE_TO_SEARCH) {
+//			String currURL;
+//			CrawlerWorker crawlerWorker = new CrawlerWorker();
+//			if (this.pagesToVisisted.isEmpty()) {
+//				currURL = url;
+//				this.pagesVisited.add(url);
+//			}
+//			else {
+//				currURL = this.nextURL();
+//			}
+//			// crawl the page of currURL
+//			int fileId = Report.getCurrUrlId();
+//			crawlerWorker.crawl(currURL, fileId);
+////			try {
+////				TimeUnit.SECONDS.sleep(3);  // wait 3 second
+////			} catch (InterruptedException e) {
+////				// TODO Auto-generated catch block
+////				e.printStackTrace();
+////			} //
+//			System.out.println("pagesVisited.size() = " + this.pagesVisited.size());
+//			
+//			this.pagesToVisisted.addAll(crawlerWorker.getLinks());
+//		}
+//		System.out.println(String.format("**Done** Visited %s web page(s)",
+//				this.pagesVisited.size()));
+//	}
+//	
+//	
+//	
+//	
+//	/**
+//	 * return the next URL to visit
+//	 * @return nextURL
+//	 */
+//	private String nextURL() {
+//		String nextURL;
+//		// make sure that nextURL has not already been visited
+//		do {
+//			nextURL = this.pagesToVisisted.remove(0);
+//		} while (this.pagesVisited.contains(nextURL));
+//		this.pagesVisited.add(nextURL);
+//		return nextURL;
+//	}
+//	
+//	
+//	
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//}
--- src/webCrawler/CrawlerWorker.java
+++ src/webCrawler/CrawlerWorker.java
@@ -38,6 +38,7 @@ public class CrawlerWorker {
 	 */
 	public boolean crawl(String url, int fileId) {
 		try {
+			System.out.println("'" + url + "'");
 			Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
 			Document htmlDocument = connection.get();
 			this.htmlDocument = htmlDocument;
@@ -59,14 +60,14 @@ public class CrawlerWorker {
 			System.out.println("Found (" + linksOnPage.size() + ") links");
 			// store the links in a private field
 			for (Element link: linksOnPage) {
-				this.links.add(link.absUrl("href"));
+				String crawledUrl = link.absUrl("href");
+				if (!crawledUrl.isEmpty()) {
+					this.links.add(crawledUrl);
+				}
 			}
 			
 			Elements imagesOnPage = htmlDocument.select("img");
-			int numofImages = imagesOnPage.size();
-			
-			
-			
+			int numofImages = imagesOnPage.size();			
 			Report.save(fileId, title, url, statusCode, numofLinks, numofImages);
 //			System.out.print(this.htmlDocument.body().text());
 			return true;
@@ -98,9 +99,9 @@ public class CrawlerWorker {
 		try {
 			String fileName = String.format("%s/%d.html", Main.REPO, fileId);
         	FileWriter fileWriter = new FileWriter(fileName, false);
-        	fileWriter.write(htmlDocument.select("a").remove().toString());
+//        	fileWriter.write(htmlDocument.select("a").remove().toString());
 
-//        	fileWriter.write(htmlDocument.toString());
+        	fileWriter.write(htmlDocument.toString());
         	fileWriter.close();
         	
         } catch (IOException e) {
@@ -111,3 +112,116 @@ public class CrawlerWorker {
 	
 
 }
+
+
+//package webCrawler;
+//
+//import java.io.BufferedWriter;
+//import java.io.File;
+//import java.io.FileWriter;
+//import java.io.IOException;
+//import java.util.LinkedList;
+//import java.util.List;
+//
+//import org.jsoup.Connection;
+//import org.jsoup.Jsoup;
+//import org.jsoup.nodes.Document;
+//import org.jsoup.nodes.Element;
+//import org.jsoup.select.Elements;
+//
+///**
+// * 1. Crawl the page(make an HTTP request and parse the page)
+// * 2. Collect all the links on that pages
+// * 3. Collect all the words on that pages (Not done)
+// * 4. Search for a word
+// * 5. Return all the links on the page
+// * @author Wendi
+// *
+// */
+//public class CrawlerWorker {
+//	// use a fake USER_AGENT so the web server thinks the robot is a normal web browser
+//	private static final String USER_AGENT = 
+//			"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.112 Safari/535.1";
+//	
+//	private List<String> links = new LinkedList<String>(); // a list of URLs
+//	private Document htmlDocument;
+//	
+//	/**
+//	 * 1. makes an HTTP request, checks the response and collects all the links on that page.
+//	 * 2. Collect words after the successful crawl.
+//	 * @param nextURL
+//	 * @return whether of not the crawl was successful
+//	 */
+//	public boolean crawl(String url, int fileId) {
+//		try {
+//			Connection connection = Jsoup.connect(url).userAgent(USER_AGENT);
+//			Document htmlDocument = connection.get();
+//			this.htmlDocument = htmlDocument;
+//			int statusCode = connection.response().statusCode();
+//			if (statusCode == 200) {  // 200 is the HTTP OK status code
+//				System.out.println("\n**Visiting**\nReceived web page at " + url);
+//				generateHtmlFile(url, fileId); 
+//			}
+//			if (!connection.response().contentType().contains("text/html")) {
+//				System.out.println("**Failure**\nRetrieved something other than HTML");
+//				return false;
+//			}
+//			
+//			String title = htmlDocument.title();
+//			System.out.println("title: " + title);
+//			
+//			Elements linksOnPage = htmlDocument.select("a[href]");
+//			int numofLinks = linksOnPage.size();
+//			System.out.println("Found (" + linksOnPage.size() + ") links");
+//			// store the links in a private field
+//			for (Element link: linksOnPage) {
+//				this.links.add(link.absUrl("href"));
+//			}
+//			
+//			Elements imagesOnPage = htmlDocument.select("img");
+//			int numofImages = imagesOnPage.size();
+//			
+//			Report.save(fileId, title, url, statusCode, numofLinks, numofImages);
+////			System.out.print(this.htmlDocument.body().text());
+//			return true;
+//		}
+//		catch (IOException ioe) {  // not successful in HTTP request
+//			return false;
+//		}
+//		
+//	}
+//	
+//	
+//	
+//	/**
+//	 * return a list of all the URLs on the pages
+//	 * @return list
+//	 */
+//	public List<String> getLinks() {
+////		List<String> list = new LinkedList<String>();
+//		return this.links;
+//	}
+//	
+//	
+//	/**
+//	 * generate html file for each page in directory
+//	 * @param url
+//	 * @param fileId
+//	 */
+//	public void generateHtmlFile(String url, int fileId) {
+//		try {
+//			String fileName = String.format("%s/%d.html", Main.REPO, fileId);
+//        	FileWriter fileWriter = new FileWriter(fileName, false);
+//        	fileWriter.write(htmlDocument.select("a").remove().toString());
+//
+////        	fileWriter.write(htmlDocument.toString());
+//        	fileWriter.close();
+//        	
+//        } catch (IOException e) {
+//			e.printStackTrace();
+//		} 
+//	}
+//	
+//	
+//
+//}
